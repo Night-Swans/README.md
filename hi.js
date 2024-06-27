46f515fa2482a7cb5b33d69ffeced0aa81c5c9cd
Evgeniy <zenyagus@yandex.ru>
2024-06-27 13:40:00 +0300

    UpdatecalculateFactorial.js
    function
    
    Co-Authored-By: Evgeniy <167186825+zhenyaguskov@users.noreply.github.com>

--- hi.js
+++ hi.js
@@ -1,1 +1,11 @@
-console.log("Bird")
+function calculateFactorial(number) {
+    if (number < 0) {
+      return 'Error: Factorial is not defined for negative numbers.';
+    } else if (number === 0) {
+      return 1;
+    } else {
+      return number * calculateFactorial(number - 1);
+    }
+  }
+  
+  console.log(calculateFactorial(5)); // Output: 120
